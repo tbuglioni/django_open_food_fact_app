ceb5dc0c9a8d2f09c1a03266741b38c063191ce3
thomas <thomas.buglioni@gmail.com>
2021-05-06 21:52:40 +0200

    0.11.0 search x2 home / substitute

--- substitute/views.py
+++ substitute/views.py
@@ -1,32 +1,56 @@
+from functools import reduce
+import operator
+
 from django.shortcuts import render, redirect
 from django.http import HttpResponseRedirect
 from .models import Nutriscore, Product, Tag
-from django.db.models import Count, Min, Max, Avg
+from django.db.models import Count, Min, Max, Avg, Q
 
 
 def substitute_views(request):
-    # get product and tags
-    current_product = Product.objects.get(name="10 œufs frais")
-    product_tags = Tag.objects.filter(products__id=current_product.id)
-
-    # get most popular tags
-    all_tag = (
-        Tag.objects.annotate(Count("products"))
-        .order_by("-products__count")
-        .filter(id__in=product_tags)
-    )
-
-    product_substitute = (
-        Product.objects.filter(
-            product_nutriscore__name=current_product.product_nutriscore.name
-        )
-        .filter(tags=all_tag[0])
-        .filter(tags=all_tag[1])
-        .filter(tags=all_tag[2])[:5]
-    )
-
     context = {}
-    context["substitute"] = product_substitute
+    srh = request.GET["query"]
+    srh = [elt for elt in srh.split(" ")]
+    # find with name
+
+    try:
+        list_query = reduce(operator.and_, (Q(name__istartswith=item) for item in srh))
+
+        current_product = Product.objects.filter(list_query)[0]
+        # current_product = Product.objects.filter(name__istartswith=srh)[0]
+        product_tags = Tag.objects.filter(products__id=current_product.id)
+
+        target_nutriscore = "a"
+        if current_product.product_nutriscore.name == "e":
+            target_nutriscore = "d"
+        elif current_product.product_nutriscore.name == "d":
+            target_nutriscore = "c"
+        elif current_product.product_nutriscore.name == "c":
+            target_nutriscore = "b"
+        elif current_product.product_nutriscore.name == "b":
+            target_nutriscore = "a"
+        else:
+            target_nutriscore = "a"
+
+        # get most popular tags
+        all_tag = (
+            Tag.objects.annotate(Count("products"))
+            .order_by("-products__count")
+            .filter(id__in=product_tags)
+        )
+
+        product_substitute = (
+            Product.objects.filter(product_nutriscore__name=target_nutriscore)
+            .filter(tags=all_tag[0])
+            .filter(tags=all_tag[1])
+            .filter(tags=all_tag[2])[:5]
+        )
+        context["substitutes"] = product_substitute
+        context["products"] = current_product
+    except IndexError:
+        context["error"] = "oula bonne question ... euh ... on a rien trouvé :/"
+
+    context["search"] = srh
 
     return render(request, "substitute/substitute.html", context)
 
@@ -52,6 +76,6 @@ def my_food_view(request):
     favorite = Product.objects.filter(users__id=user.id)
     if len(favorite) > 0:
 
-        context["favorite"] = favorite
+        context["favorites"] = favorite
 
     return render(request, "substitute/my_food.html", context)
