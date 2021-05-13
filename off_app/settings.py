8a5a063c2b1836b42d6b008a33190206ff820326
thomas <thomas.buglioni@gmail.com>
2021-05-13 16:52:59 +0200

    0.15.5 add runtime

--- off_app/settings.py
+++ off_app/settings.py
@@ -78,13 +78,6 @@ WSGI_APPLICATION = "off_app.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-# DATABASES = {
-#     "default": {
-#         "ENGINE": "django.db.backends.sqlite3",
-#         "NAME": "mydatabase",
-#     }
-# }
-
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
