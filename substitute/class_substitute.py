c04478a069c671a7fc4253057665d77428f65fb7
thomas <thomas.buglioni@gmail.com>
2021-05-20 14:24:57 +0200

    1.2.19 refactoring substitute_views

--- substitute/class_substitute.py
+++ substitute/class_substitute.py
@@ -7,77 +7,99 @@ from django.db.models import Count, Q
 
 
 class SubstituteViews:
+    """
+    perform substitute views : get x substitutes from 1 query
+    """
 
     def __query_to_list(self, query):
+        """ split query to list """
         query = uri_to_iri(query)
         self.context = {}
         self.query = query
         self.query = [elt for elt in self.query.split(" ")]
 
+    def __get_product_reference(self):
+        """ get 1 product from query as reference"""
+
+        if len(self.query) == 1:
+            self.list_query = reduce(
+                operator.and_, (
+                    Q(name__istartswith=item) |
+                    Q(name__icontains=(" ", item, " ")) |
+                    Q(name__iendswith=(" ", item)) for item in self.query)
+            )
+        else:
+            # more than 1 element in query
+            self.list_query = reduce(
+                operator.and_, (Q(name__icontains=item)
+                                for item in self.query)
+            )
+
+        # random queryset order
+        self.current_product = Product.objects.filter(
+            self.list_query).order_by('?')
+
+        # get 1 product(name/nutriscore/tag) match with query
+        self.current_product = self.current_product[0]
+        product_tags = Tag.objects.filter(
+            products__id=self.current_product.id
+        )
+        if self.current_product.product_nutriscore.name == "e":
+            self.target_nutriscore = ["a", "b", "c", "d"]
+        elif self.current_product.product_nutriscore.name == "d":
+            self.target_nutriscore = ["a", "b", "c"]
+        elif self.current_product.product_nutriscore.name == "c":
+            self.target_nutriscore = ["a", "b"]
+        elif self.current_product.product_nutriscore.name == "b":
+            self.target_nutriscore = ["a"]
+        else:
+            self.target_nutriscore = ["a"]
+
+        # from product-ref order tag by importance
+        self.all_tag = (
+            Tag.objects.annotate(Count("products"))
+            .order_by("-products__count")
+            .filter(id__in=product_tags)
+        )
+
+    def __find_substitute(self):
+        """ find substitute from product_reference OR query """
+
+        # try to get substitutes with query in name
+        self.product_substitute = Product.objects.filter(
+            product_nutriscore__name__in=self.target_nutriscore
+        ).filter(self.list_query).order_by('?')[:27]
+
+        # try to get substitutes with tag from product-ref
+        if len(self.product_substitute) <= 2:
+            self.product_substitute = (
+                Product.objects.filter(
+                    product_nutriscore__name__in=self.target_nutriscore)
+                .filter(tags=self.all_tag[0])
+                .filter(tags=self.all_tag[1])
+                .filter(tags=self.all_tag[2])
+                .order_by('?')[:27]
+            )
+
     def run_substitute(self, request, query):
+        """ get from query substitutes AND render html requests """
         self.__query_to_list(query)
-        # find with name
 
         try:
+            loop = 0
+            self.product_substitute = []
+            while len(self.product_substitute) == 0 or loop < 3:
+                self.__get_product_reference()
+                self.__find_substitute()
+                loop += 1
 
-            if len(self.query) == 1:
-                list_query = reduce(
-                    operator.and_, (
-                        Q(name__istartswith=item) |
-                        Q(name__icontains=(" ", item, " ")) |
-                        Q(name__iendswith=(" ", item)) for item in self.query)
-                )
-            else:
-                list_query = reduce(
-                    operator.and_, (Q(name__icontains=item)
-                                    for item in self.query)
-                )
-
-            current_product = Product.objects.filter(list_query).order_by('?')
-            current_product = current_product[0]
-
-            product_tags = Tag.objects.filter(
-                products__id=current_product.id
-            )
-
-            target_nutriscore = "a"
-            if current_product.product_nutriscore.name == "e":
-                target_nutriscore = ["a", "b", "c", "d"]
-            elif current_product.product_nutriscore.name == "d":
-                target_nutriscore = ["a", "b", "c"]
-            elif current_product.product_nutriscore.name == "c":
-                target_nutriscore = ["a", "b"]
-            elif current_product.product_nutriscore.name == "b":
-                target_nutriscore = ["a"]
-            else:
-                target_nutriscore = ["a"]
-
-            # get most popular tags
-            all_tag = (
-                Tag.objects.annotate(Count("products"))
-                .order_by("-products__count")
-                .filter(id__in=product_tags)
-            )
-
-            product_substitute = Product.objects.filter(
-                product_nutriscore__name__in=target_nutriscore
-            ).filter(name__icontains=self.query)
-
-            if len(product_substitute) <= 3:
-                product_substitute = (
-                    Product.objects.filter(
-                        product_nutriscore__name__in=target_nutriscore)
-                    .filter(tags=all_tag[0])
-                    .filter(tags=all_tag[1])
-                    .filter(tags=all_tag[2])[:12]
-                )
-
-            if len(product_substitute) == 0:
+            # if no product
+            if len(self.product_substitute) == 0:
                 self.context["error"] = ("oula bonne question ..."
                                          " euh ... on a rien trouvé :/")
 
-            self.context["substitutes"] = product_substitute
-            self.context["products"] = current_product
+            self.context["substitutes"] = self.product_substitute
+            self.context["products"] = self.current_product
         except IndexError:
             self.context["error"] = ("oula bonne question ..."
                                      " euh ... on a rien trouvé :/")
