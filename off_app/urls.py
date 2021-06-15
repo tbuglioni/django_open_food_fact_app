3a68cdd7f571d3ca30317a9fd7436ac87620f8f6
thomas <thomas.buglioni@gmail.com>
2021-06-15 20:51:02 +0200

    2.2.0 add reset pass (sent with email)

--- off_app/urls.py
+++ off_app/urls.py
@@ -14,6 +14,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
+
 from django.urls import path, include
 from .views import home_screen_view, mention_legal_view
 from account.views import (
@@ -30,4 +32,19 @@ urlpatterns = [
     path("account/", account_view, name="account"),
     path("substitute/", include("substitute.urls")),
     path("calcul_kcal/", include("calcul_kcal.urls")),
+
+
+
+    path("reset_password/", auth_views.PasswordResetView.as_view(template_name="account/password_reset.html"),
+         name="reset_password"),
+
+    path("reset_password_sent/", auth_views.PasswordResetDoneView.as_view(template_name="account/password_reset_sent.html"),
+         name="password_reset_done"),
+
+    path("reset/<uidb64>/<token>/", auth_views.PasswordResetConfirmView.as_view(template_name="account/password_reset_form.html"),
+         name="password_reset_confirm"),
+
+    path("reset_password_complete/", auth_views.PasswordResetCompleteView.as_view(template_name="account/password_reset_done.html"),
+         name="password_reset_complete"),
+
 ]
